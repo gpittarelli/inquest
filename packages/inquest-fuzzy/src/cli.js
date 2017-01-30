900bcc6f1c03f1a9889bdcb09cd722f3b024673d
George Pittarelli <g@gjp.cc>
2017-01-29 23:44:18 -0800

    Add inquest-fuzzy

---
+++ packages/inquest-fuzzy/src/cli.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import commander from 'commander';
+import inquirer from 'inquirer';
+import autocomplete from 'inquirer-autocomplete-prompt';
+import fuzzy from 'fuzzy';
+import {version} from '../package.json';
+
+inquirer.registerPrompt('autocomplete', autocomplete);
+
+// Usage: node _deploy_question [output-file]
+export async function fuzzyAutocomplete(message, options) {
+  const {choice} = await inquirer.prompt([{
+    type: 'autocomplete',
+    name: 'choice',
+    message,
+    source: (soFar, input) => Promise.resolve(
+      input === null ? options :
+        (fuzzy.filter(input, options) || []).map((x) => x.string)
+    ),
+    choices: []
+  }]);
+
+  return choice;
+}
+
+export default async function(argv) {
+  const command = commander
+    .version(version)
+    .description(
+  `Displays the given message and allows the user to select from the
+  specified options with fuzzy-matching autocomplete. The selected
+  value is printed to the specified output file.`)
+    .arguments('<message> <options...>')
+    .option('-o, --out-file <output file>', 'File to print selected value too')
+    .action(async (message, options, {outFile}) => {
+      fs.writeFileSync(outFile, await fuzzyAutocomplete(message, options));
+    })
+    .parse(argv);
+
+  // No arguments: (eg only ['node', '/path/to/this/cli.js'])
+  if (argv.length === 2) {
+    command.outputHelp();
+    process.exit(0);
+  }
+}
--- packages/inquest/src/cli.js
+++ packages/inquest/src/cli.js
@@ -5,6 +5,10 @@ export default function(argv) {
   const result = commander
     .version(version)
     .command('confirm', 'Ask the user Yes/No')
+    .command(
+      'fuzzy',
+      'Ask the user to select a value, autocompleted with fuzzy matching'
+    )
     .parse(argv);
 
   // No arguments: (only ['node', '/path/to/this/cli.js'])
